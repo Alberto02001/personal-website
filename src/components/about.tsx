f3b7a68f2461beb234b5fb4bb883f4ab3458a12a
Alberto2001 <a.soldiviero@be-tse.it>
2022-10-26 00:25:29 +0200

    fix about css

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -6,20 +6,24 @@ const About = () =>{
 
     return (
         <>
-            <Container className="d-flex flex-column flex-md-row aboutBack" fluid>
-                <div className="d-flex flex-column aboutBox">
+            <div className="d-flex flex-column flex-md-row aboutBack">
+                <div className="d-flex aboutBox">
+                    <div className="d-flex flex-column justify-content-center">
                     <p className="aboutTitle">ABOUT</p>
                     <p className="aboutContent">I'm Alberto, a passionate freelancer bringing you programming and design from the future. I am experienced in developing web and desktop applications including full front end design. This includes brand identity, graphics and illustrations.</p>
+                    </div>
                 </div>
-                <div className="d-flex flex-column skillsBox ms-auto">
+                <div className="d-flex skillsBox justify-content-end">
+                    <div className="d-flex flex-column justify-content-center">
                     <p className="skillsTitle">My Skills</p>
                     <p className="skillsContent">JS</p>
                     <p className="skillsContent">Angular</p>
                     <p className="skillsContent">React</p>
                     <p className="skillsContent">Photoshop</p>
                     <p className="skillsContent">Illustrator</p>
+                    </div>
                 </div>
-            </Container>
+            </div>
         </>
     )
 }
